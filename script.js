2651bca56fe7d995b98ff780d2e42459f9723ab2
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-03-15 15:04:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let searchBox = document.querySelector('#search-box');
+let images = document.querySelectorAll('.container .image-container .image');
+
+// console.log(images);
+
+searchBox.oninput = () =>{
+
+    images.forEach(hide => hide.style.display = 'none');
+
+    let value = searchBox.value;
+    
+    images.forEach(filter =>{
+        let title = filter.getAttribute('data-title');
+        if(value == title) {
+            filter.style.display = 'block';
+        }
+    });
+}
